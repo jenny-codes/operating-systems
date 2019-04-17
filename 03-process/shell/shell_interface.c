b552171e558ab246821cea4466a6b511bdf63fd8
jennyshih <jinghua.shih@gmail.com>
2019-04-17 20:44:23 +0800

    half-finished revised shell interface

--- 03-process/shell/shell_interface.c
+++ 03-process/shell/shell_interface.c
@@ -9,78 +9,71 @@
 #include <sys/wait.h>
 #include <sys/types.h>
 
-#define MAX_LINE 80 // the max length command
+#define MAX_LINE 80+1 // the max length command
 #define MAX_ARGN MAX_LINE/2
 
-int read_line(char *buf, size_t size);
-int is_empty(const char *str);
+const char * read_line(size_t size);
+bool is_empty(const char *str);
 int parse_line(char *str, char **args, int limit);
 
 int main(void) {
-    char line[MAX_LINE + 1];
-    char *args[MAX_ARGN + 1];  // command line arguemnts
-    int argn;
+		int argn;
+		int wstatus;
+    char line[MAX_LINE];
+    char *args[MAX_ARGN + 1];  
     pid_t pid;
-	int wstatus;
 
     while (true) {
         printf("jsh> ");  // jsh == jenny's shell, of course
         fflush(stdout);
 
-        if (read_line(line, MAX_LINE + 1) == EOF)
-            break;
+				line = read_line(MAX_LINE);
+				if (line == EOF || line[0] == '\0')
+						continue;
 
-        if (is_empty(line))
-            continue;
+				if ((argn = parse_line(line, args, MAX_ARGN)) == 0)
+						continue;
 
-		if ((argn = parse_line(line, args, MAX_ARGN)) == 0)
-			continue;
-
-		if (strcmp(args[0], "exit") == 0) {
-			break;
-		} 
-        else if ((pid = fork()) == 0) {
-			if (execvp(args[0], args) == -1) {
-				fprintf(stderr, "%s\n", strerror(errno));
+			if (strcmp(args[0], "exit") == 0) {
 				break;
+			} 
+			else if ((pid = fork()) == 0) {
+				if (execvp(args[0], args) == -1) {
+					fprintf(stderr, "%s\n", strerror(errno));
+					break;
+				}
+			} 
+			else {
+					waitpid(pid, &wstatus, WUNTRACED);
 			}
-		} 
-        else {
-            waitpid(pid, &wstatus, WUNTRACED);
-		}
     }
 
     return 0;
 }
 
-int read_line(char *buf, size_t size)
+const char * read_line(size_t max_size)
 {
-	size_t len;
-	int tmp;
+		int tmp;
+		char *buf
+		size_t len;
 
-	if (fgets(buf, size, stdin) == NULL)
-		return EOF;
+		if (fgets(buf, max_size, stdin) == NULL)
+				return EOF;
 
-	if ((len = strlen(buf)) > 0) {
-		if (buf[len - 1] == '\n') {
-			buf[len - 1] = '\0';
-		} 
-        else {
-			while ((tmp = getchar()) != EOF && tmp != '\n')
-				;
+		if ((len = strlen(buf)) > 0) {
+				if (buf[len - 1] == '\n') {
+					buf[len - 1] = '\0';
+				} 
+				else {
+						while(true){
+								tmp = getchar();
+								if (tmp == EOF || tmp == '\n')
+										break;
+						}
+				}
 		}
-	}
 
-	return 0;
-}
-
-int is_empty(const char *str)
-{
-	for (const char *p = str; *p; p++)
-		if (!isspace(*p))
-			return 0;
-
-	return 1;
+		return buf;
 }
 
 int parse_line(char *str, char **args, int limit)
@@ -107,7 +100,7 @@ int parse_line(char *str, char **args, int limit)
 			*q++ = *p;
 			is_escape = false;
 		} 
-        else {
+		else {
 			switch (p[0]) {
 			case '\\':
 				is_escape = true;
---
+++ 03-process/shell/shell_revisited.c
@@ -0,0 +1,46 @@
+/* reference: "https://github.com/dalmia/Operating-Systems" */
+
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef int bool;
+#define true 1
+#define false 0
+
+#define RL_BUFSIZE 1024 /* buffer size for reading user input */
+#define TOK_BUFSIZE 64 /* buffer size for splitting the arguments */
+#define HIST_SIZE 10 /* buffer size for storing history of commands */
+#define TOK_DELIM " \t\r\n\a" /* delimiters for parsing the arguments */
+
+/* global variable to check parent and child process concurrency */
+bool is_concurrent = false;
+/* global variable to point to the last command executed */
+int cur_pos = -1;
+/* global variable storing the history of commands executed */
+char *history[HIST_SIZE];
+int cur_bufsize = TOK_BUFSIZE;
+
+/* Function declarations for built-in shell commands */
+int jsh_cd(char **args);
+int jsh_help(char **args);
+int jsh_exit(char **args);
+
+/* List of built-in commands, followed by their corresponding functions */
+char *builtin_str[] = {
+    "cd",
+    "help",
+    "exit"
+};
+
+int (*builtin_func[]) (char **) = {
+    &lsh_cd,
+    &lsh_help,
+    &lsh_exit
+};
+
+int lsh_num_builtins(){
+    return sizeof(builtin_str) / sizeof(char *);
+}
